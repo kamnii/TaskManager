3794ecd68ba6fc7cf38c435260e172fec176e4b9
kamnii <kntskrbkv@gmail.com>
2026-02-20 01:27:10 +0600

    Add Login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -27,3 +28,34 @@ class RegisterView(APIView):
 
         return Response(serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+
+        if username is None or password is None:
+            return Response({'error': 'Нужен логин и пароль'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(
+            username=username,
+            password=password
+        )
+
+        if user is None:
+            return Response({'error': 'Неверные данные'},
+                     status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email
+            },
+            'access': str(refresh.access_token),
+            'refresh': str(refresh)
+        })
